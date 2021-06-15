cfaf1f7fb3f7eebc0fefd4811f3683bc2d1179d3
TarynL <tarlytle@gmail.com>
2021-06-15 14:13:00 -0500

    ability to add a journal entry

--- TabloidCLI/Repositories/JournalRepository.cs
+++ TabloidCLI/Repositories/JournalRepository.cs
@@ -95,11 +95,23 @@ FROM Journal
             }
         }
 
-        public void Insert(Journal entry)
+        public void Insert(Journal journal)
         {
-            throw new NotImplementedException();
-        }
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"INSERT INTO Journal (Title, Content, CreateDateTime )
+                                                     VALUES (@title, @content, @createDateTime)";
+                    cmd.Parameters.AddWithValue("@title", journal.Title);
+                    cmd.Parameters.AddWithValue("@content", journal.Content);
+                    cmd.Parameters.AddWithValue("@createDateTime", journal.CreateDateTime);
 
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
         public void Update(Journal entry)
         {
             throw new NotImplementedException();
