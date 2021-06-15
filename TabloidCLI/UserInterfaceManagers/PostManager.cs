8262605d28ad56266b74585725e238c09db28c3c
dudley-codes <alexdudley@me.com>
2021-06-15 12:03:05 -0500

    breaking for lunch

---
+++ TabloidCLI/UserInterfaceManagers/PostManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TabloidCLI.Repositories;
+
+namespace TabloidCLI.UserInterfaceManagers
+{
+    public class PostManager : IUserInterfaceManager
+    {
+        private readonly IUserInterfaceManager _parentUI;
+        private PostRepository _postRepository;
+        private string _connectionString;
+        public PostManager(IUserInterfaceManager parentUI, string connectionString)
+        {
+            _parentUI = parentUI;
+            _postRepository = new PostRepository(connectionString);
+            _connectionString = connectionString;
+        }
+        public IUserInterfaceManager Execute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
